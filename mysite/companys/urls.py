7da5ed665dc42ac663e9807f3ae7e37bbc18876d
paka_coding <wnstjd5408@naver.com>
2022-06-13 22:44:08 +0900

    feat: Company and JobPosting CRUD

--- mysite/companys/urls.py
+++ mysite/companys/urls.py
@@ -1,3 +1,18 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework_nested import routers
 
-urlpatterns = []
+from .views import CompanyViewSet, JobPostringViewSet
+
+router = routers.DefaultRouter()
+
+
+router.register(r"", CompanyViewSet, basename="company")
+
+job_posting_router = routers.NestedDefaultRouter(router, "", lookup="company")
+job_posting_router.register("job_postings", JobPostringViewSet, basename="job_posting")
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("", include(job_posting_router.urls)),
+]
