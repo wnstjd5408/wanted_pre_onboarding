20ef77ff6c1e87d8d8aac2c8a4fc7650741e0ded
paka_coding <wnstjd5408@naver.com>
2022-06-18 11:36:43 +0900

    feat: Company and JobPosting admin 추가

--- mysite/companys/admin.py
+++ mysite/companys/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ["id", "cp_name"]
+
+
+@admin.register(Job_Posting)
+class JobPostingAdmin(admin.ModelAdmin):
+    list_display = ["id", "jp_position", "jp_compensation"]
